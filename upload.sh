7fc52be12641600244cad6ae35e1b592c606a72e
Timothy Spann <tspann@hortonworks.com>
2018-03-16 10:47:42 -0400

    Update

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
- scp -i /Volumes/seagate/field.pem nifi-attributecleaner-nar/target/nifi-attributecleaner-nar-1.0.nar centos@princeton1.field.hortonworks.com:/opt/demo
+ scp -i /Volumes/seagate/field.pem nifi-convertjsontoddl-nar/target/nifi-convertjsontoddl-nar-1.0.nar centos@princeton1.field.hortonworks.com:/opt/demo
